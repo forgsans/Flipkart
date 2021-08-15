2754d38415b7afc801c5df5fb9a90a13e8bfd117
forgsans <forgsans@gmail.com>
2021-08-15 00:39:14 -0400

    By Sandhyagit

---
+++ src/test/java/Logout.java
@@ -0,0 +1,17 @@
+
+public class Logout {
+	
+	public void logOut()
+	{
+		System.out.println("LogOut");
+		
+	}
+
+	public static void main(String[] args)
+	{
+		Logout obj=new Logout();
+		obj.logOut();
+
+	}
+
+}
